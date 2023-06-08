5102381634d987642f6cbe7975d5c34589c1d846
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2023-06-07 18:33:43 -0700

    Updated conclusion dialogue

--- Assets/Scripts/Conclusion/ConclusionController.cs
+++ Assets/Scripts/Conclusion/ConclusionController.cs
@@ -18,35 +18,59 @@ public class ConclusionController : MonoBehaviour
     }
 
     // Runs cutscene with delay
-    private IEnumerator RunCutscene() {        
+    private IEnumerator RunCutscene() {
+        // Calculate the percentage of trash collected
+        float trash_percentage = playerData.coin_counter / playerData.total_coins;
+
         string string_1 = "Trash Collected: " + playerData.coin_counter + " / " + playerData.total_coins;
 
         // Sentence 1
-        StartCoroutine(TypeSentence(string_1));
+        StartCoroutine(TypeSentence(string_1, Color.black));
 
         // Delay after typed
         yield return new WaitForSeconds(4f);
 
-        string string_2 = "Think about how your actions in-game had an active effect of the environment!";
+        // Sentence 2
+        if (trash_percentage < 0.33f)
+        {
+            string string_2bad = "You might not have noticed, but your actions in game had an active effect on the environment...";
+            StartCoroutine(TypeSentence(string_2bad, Color.red));
+        }
 
-        // Sentence 1
-        StartCoroutine(TypeSentence(string_2));
+        else if (trash_percentage < 0.66f)
+        {
+            string string_2neutral = "Good job! You have begun to notice how your actions in-game had an active effect on the environment.";
+            StartCoroutine(TypeSentence(string_2neutral, Color.black));
+        }
 
-        // Delay after typed
-        yield return new WaitForSeconds(5f);
-
-        string string_3 = "Each decision you make in your life can have a small, but meaninful effect on the environment today!";
-
-        // Sentence 1
-        StartCoroutine(TypeSentence(string_3));
+        else {
+            string string_2good = "Amazing job! You understand how your actions in-game had an active effect of the environment!";
+            StartCoroutine(TypeSentence(string_2good, Color.green));
+        }
 
         // Delay after typed
         yield return new WaitForSeconds(7f);
 
-        string string_4 = "Thank you for playing! - David, Jake, Lauren";
+        string string_3 = "Similarly, each small decision you make in your life can have a small, but meaninful effect on the environment today!";
 
-        // Sentence 1
-        StartCoroutine(TypeSentence(string_4));
+        // Sentence 3
+        StartCoroutine(TypeSentence(string_3, Color.black));
+
+        // Delay after typed
+        yield return new WaitForSeconds(7f);
+
+        string string_4 = "Continue to replay the game to explore the different ways you can impact the environment!";
+
+        // Sentence 4
+        StartCoroutine(TypeSentence(string_4, Color.black));
+
+        // Delay after typed
+        yield return new WaitForSeconds(5f);
+
+        string string_5 = "Thank you for playing! - David, Jake, Lauren";
+
+        // Sentence 5
+        StartCoroutine(TypeSentence(string_5, Color.black));
 
         // Delay after typed
         yield return new WaitForSeconds(6f);
@@ -56,9 +80,10 @@ public class ConclusionController : MonoBehaviour
     }
 
     // Coroutine for typing a string in the dialogue box
-    IEnumerator TypeSentence(string sentence)
+    IEnumerator TypeSentence(string sentence, Color color)
     {
         conclusion_text.text = "";
+        conclusion_text.color = color;
         foreach (char letter in sentence.ToCharArray())
         {
             conclusion_text.text += letter;
